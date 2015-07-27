c9a06345cdb9ac2c983826602dac9720f4067322
Milind Singh <milindsingh84@gmail.com>
2015-07-27 08:52:20 -0700

    send mail -done..edited procfile to see both server logs and sikekiq with just 2 tabs

--- Procfile
+++ Procfile
@@ -1,4 +1,3 @@
 redis:       redis-server
-sidekiq:     bundle exec sidekiq -q DelayedMailer,1 -q default
+sidekiq:     bundle exec sidekiq -C ./config/sidekiq.yml
 sidekiq_web: bundle exec thin -R sidekiq.ru start -p 9292
-web:         bundle exec thin start
--- app/mailers/recommendation_mailer.rb
+++ app/mailers/recommendation_mailer.rb
@@ -1,21 +1,16 @@
 class RecommendationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@shopease.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.recommendation_mailer.send_recommendation.subject
   #
-  def send_recommendation(id)
-    @recommdation=ProductRecommendation.find id
+  def send_recommendation(id,reciever,account_id)
+    @recommendation=ProductRecommendation.find id
+    @account=Account.find account_id
     Rails.logger.info "==========Sending Recommendation Email=========="
-    @greeting = "Hi"
-     mailers_count=@Recommendation.recommend_to
-     mailers_count.split(",").each do |reciever|
-      mail(:to => reciever.to_s, :subject => "#{@recommdation.recommended_by} has made a Recommendation")
-    end
-
-
+    mail(:to => reciever, :subject => "#{@recommendation.recommended_by} has made a Recommendation")
   end
 
 
